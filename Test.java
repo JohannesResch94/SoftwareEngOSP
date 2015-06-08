063736f1b718a4f27b37f763810f964d253ba181
JohannesResch94 <johannes-resch@t-online.de>
2015-06-08 13:47:23 +0200

    Create Test.java

---
+++ Test.java
@@ -0,0 +1,20 @@
+public class test {
+
+	public static void main(String[] args) {
+		  
+	int i = addition(1,1);
+	int j = multi(2,2);
+	
+	}
+	
+public int addition(int k, int l){
+	//TODO
+	return;
+	}
+	
+public int multi(int k, int l){
+	//TODO
+	return;
+	}
+
+}
